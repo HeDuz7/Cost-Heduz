0473d9c0864c5bce20f1d88b88cf6149df02b82b
HeDuz7 <notluiz901@gmail.com>
2024-11-18 16:25:09 -0300

    Nav e Footer
    Signed-off-by: HeDuz7 <notluiz901@gmail.com>

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,27 +3,26 @@ import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </ul>
+      <Navbar></Navbar>
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer></Footer>
     </Router>
   );
 }
